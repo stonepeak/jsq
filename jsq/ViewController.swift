0534d110146e9a6299bd278747c76ee3d0df02f4
stonepeak <dou@Mac18.local>
2016-03-17 11:21:44 +0800

    计算器
    初步

---
+++ jsq/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  jsq
+//
+//  Created by dou on 16/3/17.
+//  Copyright © 2016年 stonepeak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var y: UITextField!
+    
+    @IBAction func add(sender: UIButton) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!x.text!.isEmpty){
+            a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty){
+            b=(y.text! as NSString).doubleValue
+        }
+        c=a+b
+        z.text="\(c)"
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
